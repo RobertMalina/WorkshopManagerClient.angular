79de72e529b452dc805f0850e90172def5e42e55
Robert <robert.malina987@gmail.com>
2019-12-09 23:20:51 +0100

    root component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'dorne-project-web-app';
-}
+export class AppComponent { }
