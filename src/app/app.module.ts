d2af22298da73328e6c6baf58af82bc1fb450e18
Robert <robert.malina987@gmail.com>
2019-12-11 23:29:12 +0100

    (course) assignement no 1 completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
+import { SuccessAlertComponent } from './../success-alert/success-alert.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { WarningAlertComponent } from '../warning-alert/warning-alert.component';
 import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
+import { ServersInfoComponent } from './servers-info/servers-info.component';
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent, ServersComponent],
-  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent,
+    ServerComponent,
+    ServersInfoComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-  styleUrls: ['server.component.css']
+  styleUrls: ['./server.component.scss']
 })
-export class ServerComponent { }
+export class ServerComponent implements OnInit {
+
+  address = '10.0.0.199';
+  status = 'offline';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  getIpv4Addr() {
+    return this.address;
+  }
+
+}
