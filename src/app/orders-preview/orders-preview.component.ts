ca73d5a9541737ac47071a3d92bb15f78a1ba5d4
Robert <robert.malina987@gmail.com>
2019-12-15 21:23:48 +0100

    orders preview component base files add

---
+++ src/app/orders-preview/orders-preview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-orders-preview',
+  templateUrl: './orders-preview.component.html',
+  styleUrls: ['./orders-preview.component.scss']
+})
+export class OrdersPreviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
