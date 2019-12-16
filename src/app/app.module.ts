e47fd341a68ff88b69d15486ef127304bd4d44c3
Robert <robert.malina987@gmail.com>
2019-12-16 23:03:51 +0100

    course components removal, order-register component renamed to align convention

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,22 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { WarningAlertComponent } from '../warning-alert/warning-alert.component';
-import { ServerComponent } from './server/server.component';
 import { ServersInfoComponent } from './servers-info/servers-info.component';
 import { OrderRegisterFormComponent } from './order-register-form/order-register-form.component';
 import { OwlDateTimeModule, OwlNativeDateTimeModule } from 'ng-pick-datetime';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { ClientService } from '../services/client-service';
+import { OrdersPreviewComponent } from './orders-preview/orders-preview.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WarningAlertComponent,
     SuccessAlertComponent,
-    ServerComponent,
     ServersInfoComponent,
-    OrderRegisterFormComponent
+    OrderRegisterFormComponent,
+    OrdersPreviewComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/services/order-service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class OrderService {
+
+  // TODO
+  port: number;
+  server: string;
+  apiRoot: string;
+
+  loading: boolean;
+
+  constructor(private httpClient: HttpClient) {
+    this.port = 4210; // 4110 -> port serwera proxy
+    this.apiRoot = 'api/orders';
+    this.server = 'localhost';
+    this.loading = false;
+  }
+
+  getOrder(id: number): Observable<Order> {
+    const url = `http://${this.server}:${this.port}/${this.apiRoot}/${id}`;
+    return this.httpClient.get(url);
+  }
+
+  getOrders(): Observable<Order> {
+    const url = `http://${this.server}:${this.port}/${this.apiRoot}`;
+    return this.httpClient.get(url);
+  }
+}
